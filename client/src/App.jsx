2ffab59a01f7d4e18446e29fa06bd1de946d962a
Aziz <siliconoxide4@gmail.com>
2024-06-03 16:37:41 +0600

    Added Client and Server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [IsVisiable, setIsVisiable] = useState(false);
+  const handleVisibale = () => {
+    console.log("FIrst");
+    setIsVisiable((prev) => !prev);
+  };
+  useEffect(() => {
+    console.log("second");
+    const checkWindowWidth = () => {
+      if (window.innerWidth >= 768) {
+        setIsVisiable(true);
+      } else {
+        if (window.innerWidth < 768 && !IsVisiable) {
+          setIsVisiable(false);
+        }
+      }
+    };
+    checkWindowWidth();
+    window.addEventListener("resize", checkWindowWidth);
+    return () => {
+      window.removeEventListener("resize", checkWindowWidth);
+    };
+  }, [IsVisiable]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid grid-cols-12 grid-rows-12 h-screen">
+        <div className="col-span-12 ">
+          <button
+            className="border-2 px-6 py-1 md:hidden"
+            onClick={() => handleVisibale()}
+          >
+            show
+          </button>
+          <h1>Header</h1>
+        </div>
+        {IsVisiable && (
+          <div className={`md:block col-span-2 row-span-12 bg-indigo-400`}>
+            div1
+          </div>
+        )}
+        <div className="col-span-12 md:col-span-8 row-span-12 bg-yellow-300">
+          div2
+        </div>
+        <div className="hidden md:block col-span-2 row-span-12 bg-red-400">
+          div3
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
